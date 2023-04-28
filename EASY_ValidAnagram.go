2e245b72e1e440192633b92229b621ac42d860aa
ayshkiri <alexshkirin@mail.ru>
2023-04-28 15:26:54 +0300

    Add solution for ValidAnagram

---
+++ EASY_ValidAnagram.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var s, t string
+	fmt.Scan(&s, &t)
+	fmt.Print(string(s[0]))
+	if len(s) != len(t) {
+		//return false
+		fmt.Println("false")
+	}
+	for len(s) > 0 {
+		t = strings.Replace(t, string(s[0]), "", 1)
+		s = strings.Replace(s, string(s[0]), "", 1)
+	}
+	if len(s) == len(t) {
+		fmt.Println("true")
+	}
+}
