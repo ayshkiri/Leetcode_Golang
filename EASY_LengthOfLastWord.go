1ead20a859d648e0e66e336877a6638e566b9077
ayshkiri <alexshkirin@mail.ru>
2023-05-05 14:17:28 +0300

    Add solution for LengthOfLastWord

---
+++ EASY_LengthOfLastWord.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := " sdf "
+	var result, lastWordLength int
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) != " " {
+			lastWordLength++
+		} else if lastWordLength != 0 {
+			result = lastWordLength
+			lastWordLength = 0
+		}
+	}
+	if lastWordLength != 0 {
+		result = lastWordLength
+	}
+	fmt.Println("Length of last word 's' is ", result)
+}
